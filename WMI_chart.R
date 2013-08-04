07d83859433b01f869b50ebe190ddc68b839c8e0
Petr Bouchal <pbouchal@gmail.com>
2013-08-04 18:40:18 +0100

    Project structure tidy-up

---
+++ WMI_chart.R
@@ -0,0 +1,57 @@
+source('./src/acses_lib.R')
+
+# Load data ---------------------------------------------------------------
+
+origdata <- read.csv('./data-input/WMI.csv')
+uu <- origdata
+
+# Process data ------------------------------------------------------------
+
+uu <- melt(uu)
+uu$grp <- paste0(uu$variable,uu$Dept)
+uu$Month <- as.Date(uu$Month,format='%d/%m/%Y')
+uu$Month <- as.POSIXct(uu$Month)
+
+wmibase <- data.frame('Dept'=uu$Dept[uu$Month==min(uu$Month)],
+                      'base'=uu$value[uu$Month==min(uu$Month)],
+                      'variable'=uu$variable[uu$Month==min(uu$Month)])
+uu <- merge(uu,wmibase)
+uu$valuepc <- (uu$value-uu$base)/uu$base
+
+
+# Filter
+uu <- uu[uu$Dept!='W Office',]
+uu <- uu[uu$Dept!='S Office',]
+uu <- uu[uu$Dept!='AG Depts',]
+
+# Setup plot --------------------------------------------------------------
+
+uu$yvar <- uu$valuepc
+plottitle <- ''
+plotname <- 'plot_WMI'
+ylabel <- 'Change March 2012-March 2013 (%)'
+xlabel <- NULL
+
+ylimits <- c(min(uu$yvar,na.rm=TRUE),max(uu$yvar,na.rm=TRUE))
+
+# Build plot --------------------------------------------------------------
+
+plot_WMI <- ggplot(uu,aes(x=Month,y=yvar)) +
+  geom_hline(y=0, colour=IfGcols[1,2]) +
+  geom_line(aes(colour=variable,group=grp),size=.6) +
+  #scale_x_datetime(labels=date_format('%Y-%m'),breaks=date_breaks(width = "3 months"))+
+  scale_x_datetime(labels=c('Apr\n2012','Jul\n2012','Oct\n2012','Jan\n2013'))+
+  scale_colour_manual(values=c(IfGcols[3,1],IfGcols[2,1]),
+                      labels=c('Payroll staff (FTE)','Pay bill (payroll only)')) +
+  scale_y_continuous(labels=percent,limits=ylimits,breaks=c(-.3,-.15,0,.15,.3)) +
+  facet_wrap(~Dept,nrow=3,scales='fixed') +
+  guides(colour=guide_legend(keywidth=unit(1,'cm'))) +
+  labs(x=xlabel,y=ylabel) +
+  theme(panel.border=element_rect(fill=NA,colour=IfGcols[1,2]),
+        plot.title=element_blank(),
+        axis.ticks=element_line(colour=IfGcols[1,2]),
+        panel.grid.major.y=element_line(colour=IfGcols[1,3]),
+        axis.text=element_text(size=8))
+plot_WMI
+
+SavePlot(plotname=plotname,plotformat=plotformat,ploth=ph,plotw=pw,ffamily=fontfamily)
